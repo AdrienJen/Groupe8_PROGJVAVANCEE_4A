50b8f8100d975d8c2f93990816220bdc132015c9
AdrienJen <adrien.jen@outlook.fr>
2021-09-30 15:15:00 +0200

    mctsv2

--- NidCahtt/Assets/MCTS.cs
+++ NidCahtt/Assets/MCTS.cs
@@ -8,12 +8,19 @@ public class MCTS
     private MCTS parent;
     public List<MCTS> child;
     public List<MCTS> availableMoves;
-    private int boardxmin = -15;
-    private int boardmax = 15;
-    private int p1randomx = -5;
-    private int p1randomy = -7;
-    private int p1randomweapon = -4;
-    private 
+    private int boardMinX = -15;
+    private int boardMaxX = 15;
+    private int p1RandomPosX = -5;
+    private int p1RandomPosY = -7;
+    private int p1RandomPosWeaponX = -4;
+    private int p1RandomPosWeaponY = -6;
+    private int p2MCTSPosX = 5;
+    private int p2MCTSPosY = -7;
+    private int p2MCTSPosWeaponX = 4;
+    private int p2MCTSPosWeaponY = -6;
+    private int jump = 4;
+    private int numberOfMove=4;
+    private int actualMove;
     void Start()
     {
         
@@ -28,8 +35,59 @@ public class MCTS
     void ComputeMCTS()
     {
         int max = 20;
-        Action bestaction = null;
+        for(actualMove=1;actualMove<=numberOfMove;actualMove++)
+        {
+            for (int x = 1; x <= 20; x++)
+            {
+                if (actualMove == 1)
+                {
+                    p2MCTSPosX -= 2;
+                    p2MCTSPosWeaponX = p2MCTSPosX -1;
+                    if (p2MCTSPosX <= boardMinX)
+                    {
+                        p2MCTSPosX = boardMinX;
+                    }
+                }
+                
+                if (actualMove == 2)
+                {
+                    p2MCTSPosX += 2;
+                    p2MCTSPosWeaponX = p2MCTSPosX +1;
+                    if (p2MCTSPosX >= boardMaxX)
+                    {
+                        p2MCTSPosX = boardMaxX;
+                    }
+                }
+                if (actualMove == 3)
+                {
+                   
+                    //Après 1 seconde augmente p2MCTSPosY de 4
+                    //while p2MCTSPosY <  p2MCTSPosY+4  ;
+                    p2MCTSPosY += 4;
+                    
+                    p2MCTSPosWeaponY = p2MCTSPosY + 1;
+                    //Après 1 seconde baisse p2MCTSPosY de 4 
+                    //while p2MCTSPosY >  p2MCTSPosY-4  ;
+                    
+                    p2MCTSPosY -= 4;
+                    
+                    
+                }
 
+                
+            }
+            
+        }
+      
+
+    }
+
+    void Move(int moveSet)
+    {
+        if (moveSet == 1)
+        {
+            
+        }
     }
     
 }
